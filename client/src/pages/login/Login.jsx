53b194f25043d0a9ce27b6f64acb952806ac40a3
Chandan Kumar <chandankrr.91@gmail.com>
2023-01-25 01:32:03 +0530

    designed login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,34 @@
 import './login.scss';
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <h1>WeShare</h1>
+      <div class="center">
+        <h1>Login</h1>
+        <form>
+          <div>
+            <input type="text" required />
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div>
+            <input type="password" required />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <div class="password" style={{ border: 'none' }}>
+            Forgot Password?
+          </div>
+          <button>Login</button>
+        </form>
+
+        <div class="signup-link" style={{ border: 'none' }}>
+          Don't have account? <button>Register</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
