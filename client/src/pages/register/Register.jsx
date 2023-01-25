9860c5684e05cb4560ea042472d41cfd6bde64e0
Chandan Kumar <chandankrr.91@gmail.com>
2023-01-25 10:28:12 +0530

    design register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,41 @@
 import './register.scss';
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <h1>WeShare</h1>
+      <div class="center">
+        <h1>Register</h1>
+        <form>
+          <div>
+            <input type="text" required />
+            <span></span>
+            <label>Name</label>
+          </div>
+          <div>
+            <input type="text" required />
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div>
+            <input type="email" required />
+            <span></span>
+            <label>Email</label>
+          </div>
+          <div>
+            <input type="password" required />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <button>Login</button>
+        </form>
+
+        <div class="signup-link" style={{ border: 'none' }}>
+          Already have account? <button>Signin</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
