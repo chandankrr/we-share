b630c05f3143c3c4d7988fb356a4b10c40665b6d
Chandan Kumar <chandankrr.91@gmail.com>
2023-01-28 12:46:46 +0530

    added functionality to register using register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,32 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './register.scss';
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:8800/api/auth/register', inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
   return (
     <div className="register">
       <h1>WeShare</h1>
@@ -9,26 +34,37 @@ const Register = () => {
         <h1>Register</h1>
         <form>
           <div>
-            <input type="text" required />
+            <input type="text" required name="name" onChange={handleChange} />
             <span></span>
             <label>Name</label>
           </div>
           <div>
-            <input type="text" required />
+            <input
+              type="text"
+              required
+              name="username"
+              onChange={handleChange}
+            />
             <span></span>
             <label>Username</label>
           </div>
           <div>
-            <input type="email" required />
+            <input type="email" required name="email" onChange={handleChange} />
             <span></span>
             <label>Email</label>
           </div>
           <div>
-            <input type="password" required />
+            <input
+              type="password"
+              required
+              name="password"
+              onChange={handleChange}
+            />
             <span></span>
             <label>Password</label>
           </div>
-          <button>Register</button>
+          {err && err}
+          <button onClick={handleClick}>Register</button>
         </form>
 
         <div className="signin-link" style={{ border: 'none' }}>
