7f7409b7bfc68ad16807ff8e9b3e3ffe0dd1909b
Chandan Kumar <chandankrr.91@gmail.com>
2023-01-28 16:06:19 +0530

    added functionality to login using login form

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,13 +1,30 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/authContext';
 import './login.scss';
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: '',
+  });
+  const [err, setErr] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   const { login } = useContext(AuthContext);
 
-  const handleLogin = () => {
-    login();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate('/');
+    } catch (err) {
+      setErr(err.response.data);
+    }
   };
 
   return (
@@ -17,18 +34,19 @@ const Login = () => {
         <h1>Login</h1>
         <form>
           <div>
-            <input type="text" required />
+            <input type="text" name="username" onChange={handleChange} />
             <span></span>
             <label>Username</label>
           </div>
           <div>
-            <input type="password" required />
+            <input type="password" name="password" onChange={handleChange} />
             <span></span>
             <label>Password</label>
           </div>
           <div className="password" style={{ border: 'none' }}>
             Forgot Password?
           </div>
+          {err && err}
           <button onClick={handleLogin}>Login</button>
         </form>
 
