7964c6dfd859ed76e5045ac282c0431ebf36577a
Chandan Kumar <chandankrr.91@gmail.com>
2023-02-01 13:00:53 +0530

    added the functionality to like on posts and delete own post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,24 +5,68 @@ import {
   ShareOutlined,
   TextsmsOutlined,
 } from '@mui/icons-material';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import moment from 'moment';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { makeRequest } from '../../axios';
+import { AuthContext } from '../../context/authContext';
 import Comments from '../comments/Comments';
 import './post.scss';
 
 const Post = ({ post }) => {
   const [commentOpen, setCommentOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  //TEMPORARY
-  const liked = false;
+  const { currentUser } = useContext(AuthContext);
+
+  const { isLoading, data } = useQuery(['likes', post.id], () =>
+    makeRequest.get('/likes?postId=' + post.id).then((res) => {
+      return res.data;
+    })
+  );
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (liked) => {
+      if (liked) return makeRequest.delete('/likes?postId=' + post.id);
+      return makeRequest.post('/likes', { postId: post.id });
+    },
+    {
+      onSuccess: () => {
+        // invalidate and refresh
+        queryClient.invalidateQueries(['likes']);
+      },
+    }
+  );
+
+  const deleteMutation = useMutation(
+    (postId) => {
+      return makeRequest.delete('/posts/' + postId);
+    },
+    {
+      onSuccess: () => {
+        // invalidate and refresh
+        queryClient.invalidateQueries(['posts']);
+      },
+    }
+  );
+
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id));
+  };
+
+  const handleDelete = () => {
+    deleteMutation.mutate(post.id);
+  };
 
   return (
     <div className="post">
       <div className="container">
         <div className="user">
           <div className="userInfo">
-            <img src={post.profilePic} alt="" />
+            <img src={'/upload/' + post.profilePic} alt="" />
             <div className="details">
               <Link
                 to={`/profile/${post.userId}`}
@@ -33,7 +77,10 @@ const Post = ({ post }) => {
               <span className="date">{moment(post.createdAt).fromNow()}</span>
             </div>
           </div>
-          <MoreHoriz />
+          <MoreHoriz onClick={() => setMenuOpen(!menuOpen)} />
+          {menuOpen && post.userId === currentUser.id && (
+            <button onClick={handleDelete}>delete</button>
+          )}
         </div>
         <div className="content">
           <p>{post.desc}</p>
@@ -41,12 +88,17 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <div className="item">
-            {liked ? (
-              <FavoriteOutlined style={{ color: '#f0544f' }} />
+            {isLoading ? (
+              'loading'
+            ) : data?.includes(currentUser.id) ? (
+              <FavoriteOutlined
+                style={{ color: '#f0544f' }}
+                onClick={handleLike}
+              />
             ) : (
-              <FavoriteBorderOutlined />
+              <FavoriteBorderOutlined onClick={handleLike} />
             )}
-            12 Likes
+            {data?.length} Likes
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlined />
