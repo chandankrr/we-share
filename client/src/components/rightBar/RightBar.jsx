9ab15b5f8a005cb08cd42b7373116b94567cfeeb
Chandan Kumar <chandankrr.91@gmail.com>
2023-02-03 00:27:52 +0530

    updated latest activities section

--- client/src/components/rightBar/RightBar.jsx
+++ client/src/components/rightBar/RightBar.jsx
@@ -1,11 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
+import { useContext } from 'react';
 import Neelu from '../../assets/neelu.jpg';
 import Ravi from '../../assets/ravi.jpg';
 import Shweta from '../../assets/shweta.jpg';
 import { makeRequest } from '../../axios';
+import { AuthContext } from '../../context/authContext';
 import './rightBar.scss';
 
 const RightBar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   // eslint-disable-next-line no-unused-vars
   const { isLoading, error, data } = useQuery(['randomUser'], () =>
     makeRequest.get('/randomUser').then((res) => {
@@ -45,33 +49,39 @@ const RightBar = () => {
         </div>
         <div className="item">
           <span>Latest Activities</span>
-          <div className="user">
-            <div className="userInfo">
-              <img src={Neelu} alt="" />
-              <p>
-                <span>Neelu</span> changed her cover picture
-              </p>
+          {currentUser.username === 'chandankrr' && (
+            <div className="user">
+              <div className="userInfo">
+                <img src={Neelu} alt="" />
+                <p>
+                  <span>Neelu</span> changed her cover picture
+                </p>
+              </div>
+              <span>4 hours ago</span>
             </div>
-            <span>4 hours ago</span>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img src={Shweta} alt="" />
-              <p>
-                <span>Shweta</span> added new post
-              </p>
+          )}
+          {currentUser.username === 'chandankrr' && (
+            <div className="user">
+              <div className="userInfo">
+                <img src={Shweta} alt="" />
+                <p>
+                  <span>Shweta</span> added new post
+                </p>
+              </div>
+              <span>2 days ago</span>
             </div>
-            <span>2 days ago</span>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img src={Ravi} alt="" />
-              <p>
-                <span>Ravi</span> updated his profile picture
-              </p>
+          )}
+          {currentUser.username === 'chandankrr' && (
+            <div className="user">
+              <div className="userInfo">
+                <img src={Ravi} alt="" />
+                <p>
+                  <span>Ravi</span> updated his profile picture
+                </p>
+              </div>
+              <span>6 days ago</span>
             </div>
-            <span>6 days ago</span>
-          </div>
+          )}
         </div>
         <div className="item">
           <span>Friends</span>
